7043952b68bf62805f96bebcf6fd475d520a4a45
Brett Slaski <brett@thatconference.com>
2020-08-31 16:47:10 -0500

    first pass on community stats

---
+++ src/dataSources/cloudFirestore/members.js
@@ -0,0 +1,25 @@
+import debug from 'debug';
+import * as Sentry from '@sentry/node';
+
+const dlog = debug('that:api:events:datasources:firebase:members');
+
+const collectionName = 'members';
+
+const member = dbInstance => {
+  dlog('instance created');
+
+  const membersCollection = dbInstance.collection(collectionName);
+
+  async function getMemberTotal() {
+    dlog('total member count');
+    return membersCollection
+      .where('isDeactivated', '==', false)
+      .select('firstName')
+      .get()
+      .then(snap => snap.size);
+  }
+
+  return { getMemberTotal };
+};
+
+export default member;
