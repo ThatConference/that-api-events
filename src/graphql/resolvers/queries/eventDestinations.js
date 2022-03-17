e65a86a4fee848e1e8bc6e0e744ff8cdfa07ebca
Brett Slaski <brett@thatconference.com>
2022-03-17 16:30:13 -0500

    Fix lint error

--- src/graphql/resolvers/queries/eventDestinations.js
+++ src/graphql/resolvers/queries/eventDestinations.js
@@ -1,7 +1,3 @@
-import debug from 'debug';
-
-const dlog = debug('that:api:eventAdmin:query');
-
 export const fieldResolvers = {
   EventDestinationsQuery: {
     destination: ({ eventId }, { name: destinationName }) => ({
