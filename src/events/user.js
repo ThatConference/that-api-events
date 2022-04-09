af411bf04e8ccdb2a78c7c5c8412093a30f6e45d
Brett Slaski <brett@thatconference.com>
2022-04-09 10:40:38 -0500

    refactor error capture in event

--- src/events/user.js
+++ src/events/user.js
@@ -13,7 +13,7 @@ function userEvents(postmark) {
   const userEventEmitter = new EventEmitter();
   dlog('user event emitter created');
 
-  async function getMemberInfo({ memberId, firestore }) {
+  async function getMemberInfo({ memberId, firestore, caller }) {
     dlog('fetching member info for %s', memberId);
     let member = null;
     try {
@@ -27,11 +27,11 @@ function userEvents(postmark) {
         scope.setLevel(Sentry.Severity.Warning);
         scope.setTags({
           event: 'user',
-          emit: constants.THAT.USER_EVENTS.REGISTRATION_CHECKIN,
+          caller,
           eventMemberId: memberId,
         });
         scope.setContext('member', member);
-        Sentry.captureMessage(`Unable to send welcome email, member not found`);
+        Sentry.captureMessage(`Unable to send email, member not found`);
       });
       return undefined;
     }
@@ -48,7 +48,11 @@ function userEvents(postmark) {
     dlog('sendCheckinWelcomeEmail called for %s', memberId);
     // may be an unallocated ticket and doesn't have a memberId assigned
     if (!memberId) return undefined;
-    const member = await getMemberInfo({ memberId, firestore });
+    const member = await getMemberInfo({
+      memberId,
+      firestore,
+      caller: 'sendCheckinWelcomeEmail',
+    });
     if (!member) return undefined;
 
     return postmark.sendEmailWithTemplate({
@@ -81,7 +85,11 @@ function userEvents(postmark) {
     );
 
     if (!memberId) return undefined;
-    const member = await getMemberInfo({ memberId, firestore });
+    const member = await getMemberInfo({
+      memberId,
+      firestore,
+      caller: 'sendSpeakerEnrollmentCompleteEmail',
+    });
     if (!member) return undefined;
 
     let templateAlias = 'speaker-enroll-complete-agree-true';
